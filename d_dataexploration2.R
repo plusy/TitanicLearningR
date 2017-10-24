5d8f9f822de2eeab49188930a42f495be820825e
Sean <56526l889@gmail.com>
2017-10-24 01:36:41 -0400

    start to work on swamysm's kernal

--- d_dataexploration2.R
+++ d_dataexploration2.R
@@ -1,3 +1,34 @@
+VAR_BEFORE <- ls()
+
+#  ------------------------------------------------------------------------
+# Blog post: Logistic Regressions and Subset Selection for the Titanic Kaggle Competition
+# Source: https://www.kaggle.com/swamysm/random-forest-logistic-r-decison-t-svm-for-titanic
+# Data Exploration part
+#  ------------------------------------------------------------------------
+
+# !diagnostics off
 
 
-https://www.kaggle.com/swamysm/random-forest-logistic-r-decison-t-svm-for-titanic
+# Localize working data frame ---------------------------------------------
+full_titanic <- getRawBaseDf()
+
+train.tit <- full_titanic %>% filter(DataTag==DATA_TAG_TRAIN)
+test.tit <- full_titanic %>% filter(DataTag==DATA_TAG_TEST)
+
+
+# Check the structure -----------------------------------------------------
+str(full_titanic)
+full_titanic
+summary(full_titanic)
+
+
+###is there any Missing obesrvation
+colSums(is.na(full_titanic))
+####Empty data
+colSums(full_titanic=='')
+##Summary shows, Age missing 263 value, Cabin too having lot of missing value and embarked just 2
+
+
+# Missing value imputation ------------------------------------------------
+###Lets replace Embarked by most frequest observation 
+table(full_titanic$Embarked)
